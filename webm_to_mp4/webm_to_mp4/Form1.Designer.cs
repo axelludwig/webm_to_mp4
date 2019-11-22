636d85fd36b3c3b28fda34ac66ededd91457ffb2
axelludwig <axel.ludwig@outlook.com>
2019-11-21 21:53:51 -0800

    v1

---
+++ webm_to_mp4/webm_to_mp4/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace webm_to_mp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "webm";
+            this.openFileDialog1.FileName = "file.webm";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.RestoreDirectory = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BackgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BackgroundWorker1_RunWorkerCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open file explorer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(154, 37);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(431, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "output folder";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(253, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(332, 20);
+            this.textBox1.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.textBox1, "If empty, mp4s will be saved in the the input folder");
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutomaticDelay = 100;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(62, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "start conversion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "temp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 69);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "webm to mp4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ webm_to_mp4/webm_to_mp4/Form1.cs
@@ -0,0 +1,107 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace webm_to_mp4
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        int count;
+        int total;
+        bool error;
+
+        public Form1()
+        {            
+            InitializeComponent();
+            label2.Text = String.Empty;
+        }
+
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            error = false;
+            count = 0;
+            total = openFileDialog1.FileNames.Length;
+            string name;
+
+            foreach (String file in openFileDialog1.FileNames)
+            {
+                if (file.Split('.').Length - 1 != 1 || file.Split('.')[1] != "webm")
+                {
+                    error = true;
+                    break;
+                }
+                name = file.Split('\\')[file.Split('\\').Length - 1].Split('.')[0];
+                FFMpegConverter ffMpeg = new FFMpegConverter();
+                ffMpeg.ConvertMedia(file, path + name + ".mp4", Format.mp4);
+                backgroundWorker1.ReportProgress(count * 100 / total);
+                ++count;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            path = string.Empty;
+
+            openFileDialog1.ShowDialog();
+            openFileDialog1.InitialDirectory = @"C:\";                       
+            if (textBox1.Text == String.Empty && openFileDialog1.FileNames.Length != 0) {
+                var tab = openFileDialog1.FileNames[0].Split('\\');
+                for (int i = 0; i < tab.Length - 1; ++i) path += tab[i] + '\\';
+            }
+            textBox1.Text = path;
+        }
+
+        private void BackgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            label2.Text = count + "/" + total;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != String.Empty)
+            {
+                label2.Text = "loading ...";
+                progressBar1.Value = 0;
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else MessageBox.Show("No file selected.");
+        }
+
+        private string Reverse(string s)
+        {
+            string res = String.Empty;
+            for (int i = s.Length - 1; i >= 0; --i) res += s[i];
+            return res;
+        }
+
+        private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (error)
+            {
+                MessageBox.Show("something went wrong ... input files must be webms and there must be only 1 point in the filename");
+                progressBar1.Value = 0;
+                label2.Text = 0 + "/" + 0;
+                return;
+            }
+
+            progressBar1.Value = 100;
+            label2.Text = total + "/" + total;
+
+            DialogResult dialogResult = MessageBox.Show(count + " mp4 file created in this folder : " + path + ". do you want to go there ?", "it worked :)", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Process.Start(path);
+            }
+        }
+    }
+}
